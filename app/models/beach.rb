a7902cf4864fa9e19c3b096155625e0b8545633c
Lubava <luba.litvinova@gmail.com>
2016-10-24 18:36:32 -0700

    added dependencies to models files

--- app/models/beach.rb
+++ app/models/beach.rb
@@ -1,3 +1,5 @@
 class Beach < ActiveRecord::Base
   belongs_to :address
+  has_many :reviews through :users
+  has_many :favorites through :users
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
   belongs_to :address
+  has_many :reviews
+  has_many :favorites
   has_secure_password
 end
